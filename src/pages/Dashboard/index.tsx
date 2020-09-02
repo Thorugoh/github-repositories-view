e6f98043ea0eb34a7a7892a979051625d6e22803
Victor Hugo <pavictorhugo@gmail.com>
2020-09-02 06:51:01 -0300

    persist repos to localstorage

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, FormEvent } from 'react';
+import React, { useState, useEffect, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 import api from '../../services/api';
 
@@ -18,8 +18,20 @@ interface Repository {
 const Dashboard: React.FC = () => {
     const [newRepo, setNewRepo] = useState('');
     const [inputError, setInputError] = useState('');
-    const [repostiories, setRepositories] = useState<Repository[]>([]);
+    const [repositories, setRepositories] = useState<Repository[]>(()=> {
+        const storagedRepositories = localStorage.getItem('@githubExplorer:repositories');
+
+        if(storagedRepositories)
+            return JSON.parse(storagedRepositories);
+        else
+            return[];
+        
+    });
     
+    useEffect(() => {
+        localStorage.setItem('@githubExplorer:repositories', JSON.stringify(repositories))
+    }, [repositories])
+
     async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
         event.preventDefault();
         
@@ -33,7 +45,7 @@ const Dashboard: React.FC = () => {
         
             const repository = response.data;
     
-            setRepositories([...repostiories, repository]);
+            setRepositories([...repositories, repository]);
             setNewRepo('');
             setInputError('');
         }catch(err) {
@@ -57,7 +69,7 @@ const Dashboard: React.FC = () => {
             </Form>
             {inputError && <Error>{inputError}</Error>}
             <Repositories>
-                {repostiories.map(repository => (
+                {repositories.map(repository => (
                     <a key={repository.full_name} href="teste">
                     <img 
                         src={repository.owner.avatar_url} 
